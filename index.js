f7a76ce9c5d8432da47a24a298bd7b138f05588b
Atharva Kulkarni <atharvakulkarni172003@gmail.com>
2023-11-27 17:06:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 // Function to log your details in the terminal
 function logDetails() {
-    const message = "Hey there! I'm Haimantika, a developer advocate at Appwrite!";
-    const twitterLink = "https://twitter.com/HaimantikaM";
-    const linkedinLink = "https://www.linkedin.com/in/haimantika-mitra/";
-    const wesbiteLink = "https://haimantika.com";
+    const message = "Hey there! I'm Atharva Kulkarni, a AI developer at TechMahindra!";
+    const twitterLink = "https://twitter.com/Atharva172003";
+    const linkedinLink = "https://www.linkedin.com/in/atharva-kulkarni-0b2415207";
+    const githubLink = "https://github.com/Atharva1723";
   
     // Create a colorful box using ANSI escape codes
     const colorfulBox = `  \x1b[38;5;51m+---------------------------------------------------------------+
@@ -12,7 +12,7 @@ function logDetails() {
     +---------------------------------------------------------------+
     | \x1b[38;5;93mTwitter:\x1b[0m \x1b[38;5;39m${twitterLink}                     \x1b[38;5;51m|
     | \x1b[38;5;93mLinkedIn:\x1b[0m \x1b[38;5;39m${linkedinLink}                 \x1b[38;5;51m|
-    | \x1b[38;5;93mPortfolio:\x1b[0m  \x1b[38;5;39m${wesbiteLink}                      \x1b[38;5;51m|
+    | \x1b[38;5;93mPortfolio:\x1b[0m  \x1b[38;5;39m${githubLink}                      \x1b[38;5;51m|
     +----------------------------------------------------------------+\x1b[0m`;
   
     // Log the colorful box in the terminal
